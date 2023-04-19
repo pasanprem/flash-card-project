fa64b8a8d321efba77a04ca64aabd1bdd35e30fb
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-04-19 09:26:21 +0100

    Can have new words displayed on canvas. Could not get to delete the old word.

--- main.py
+++ main.py
@@ -45,7 +45,27 @@
 
 from tkinter import *
 
+import pandas.core.tools.datetimes
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+############ Task 2 - Unguided #############################
+
+import random
+import pandas as pd
+
+word_list = pd.read_csv("data/french_words.csv")
+word_dict = pandas.DataFrame.to_dict(word_list)
+# print(word_dict)
+
+
+def get_french_word():
+    index = random.randint(0,99)
+    global word_dict
+    word = word_dict["French"][index]
+    # print(word)
+    canvas.create_text(400, 263, text=word, font=("Arial", 60, "bold"))
+
 
 # ---------------GENERATE INTERFACE ---------------- #
 
@@ -58,22 +78,25 @@ card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_img)
 
 canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
-canvas.create_text(400,263,text="word",font=("Arial",60,"bold"))
+# canvas.create_text(400,263,text="word",font=("Arial",60,"bold")) #Moved to the function
+get_french_word()
+
 
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 canvas.grid(row=0,column=0,columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image,highlightthickness=0)
+unknown_button = Button(image=cross_image,highlightthickness=0,command=get_french_word)
 unknown_button.grid(row=1,column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image,highlightthickness=0)
+known_button = Button(image=check_image,highlightthickness=0,command=get_french_word)
 known_button.grid(row=1,column=1)
 
+
+
+
+
+get_french_word()
+
 window.mainloop()
-
-############ Task 2 - Unguided #############################
-
-
-
